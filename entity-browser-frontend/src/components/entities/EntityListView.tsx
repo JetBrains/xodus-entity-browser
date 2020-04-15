7145d87a71b119923a5e95e8c1ae8f9dbe9bfe13
lehvolk <alexey.volkov@jetbrains.com>
2020-04-15 11:02:52 +0300

    more about entities list

--- entity-browser-frontend/src/components/entities/EntityListView.tsx
+++ entity-browser-frontend/src/components/entities/EntityListView.tsx
@@ -1,9 +1,12 @@
 import React, {Component, ReactNode} from "react";
 import {EntityLink, EntityView} from "../../api/backend-types";
 import {
+  Box,
+  Button,
+  ButtonGroup,
   Chip,
   ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary,
-  Grid,
+  Grid, IconButton,
   Paper,
   Table,
   TableBody,
@@ -13,6 +16,9 @@ import {
   Typography
 } from "@material-ui/core";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import DeleteIcon from '@material-ui/icons/Delete';
+import PencilIcon from '@material-ui/icons/Create';
+import DehazeIcon from '@material-ui/icons/Dehaze';
 
 interface EntityListViewProps {
   entity: EntityView
@@ -21,7 +27,7 @@ interface EntityListViewProps {
 class EntityListView extends Component<EntityListViewProps> {
 
   state = {
-    linksExpanded: false
+    expanded: false
   }
 
   constructor(props: EntityListViewProps) {
@@ -36,99 +42,96 @@ class EntityListView extends Component<EntityListViewProps> {
           </div>
       )
     }
-    return (<ExpansionPanel square TransitionProps={{unmountOnExit: true}} defaultExpanded={true}>
-          <ExpansionPanelSummary
-              aria-controls="panel1d-content"
-              id={"entity-properties-" + entity.id}
-              expandIcon={<ExpandMoreIcon/>}>
-            <Typography variant={"h6"}>Properties</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Table size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell align="right">Type</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {entity.properties.map((property) => (
-                    <TableRow key={property.name}>
-                      <TableCell component="th" scope="row">
-                        {property.name}
-                      </TableCell>
-                      <TableCell align="right">{property.type.displayName}</TableCell>
-                      <TableCell>{property.value}</TableCell>
-                    </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-    )
+    return (<Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell align="right">Type</TableCell>
+          <TableCell>Value</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {entity.properties.map((property) => (
+            <TableRow key={property.name}>
+              <TableCell component="th" scope="row">
+                {property.name}
+              </TableCell>
+              <TableCell align="right">{property.type.displayName}</TableCell>
+              <TableCell>{property.value}</TableCell>
+            </TableRow>
+        ))}
+      </TableBody>
+    </Table>)
   }
 
-  private static renderLinks(entity: EntityView) {
+  private static renderLinksAndBlobs(entity: EntityView) {
     if (!entity.links.length) {
       return (<div>
-        No links
+        <Typography>Links</Typography>
+        <div>No links</div>
       </div>)
     }
-    return (<ExpansionPanel square TransitionProps={{unmountOnExit: true}}>
-          <ExpansionPanelSummary
-              aria-controls="panel1d-content"
-              id={"entity-links-" + entity.id}
-              expandIcon={<ExpandMoreIcon/>}>
-            <Typography>Links</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Table size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {entity.links.map((link) => (
-                    <TableRow key={link.name}>
-                      <TableCell component="th" scope="row">
-                        {link.name}
-                      </TableCell>
-                      <TableCell>
-                        {link.entities.map((entityLink: EntityLink) => (
-                            <Chip size="small" clickable label={entityLink.label}
-                                  color={entityLink.notExists ? "secondary" : "default"}/>
-                        ))}
-                      </TableCell>
-                    </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-    )
+    return (
+        <div>
+          <Typography>Links</Typography>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Link</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {entity.links.map((link) => (
+                  <TableRow key={link.name}>
+                    <TableCell component="th" scope="row">
+                      {link.name}
+                    </TableCell>
+                    <TableCell>
+                      {link.entities.map((entityLink: EntityLink) => (
+                          <Chip size="small" clickable label={entityLink.label}
+                                color={entityLink.notExists ? "secondary" : "default"}/>
+                      ))}
+                    </TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>)
   }
 
   render() {
     const entity = this.props.entity
+    const expanded = this.state.expanded;
     return (
         <Paper key={entity.id}>
-          <Grid container spacing={2}>
+          <Grid container spacing={2} className={"entity-list-view"}>
             <Grid container item xs={12}>
               <Grid item xs={8}>
                 <Typography variant={"h6"}>{entity.label}</Typography>
               </Grid>
               <Grid item xs={4}>
-                view/edit
+                <ButtonGroup size="small" variant="outlined" color="primary"
+                             aria-label="full-width contained primary button group"
+                             className={"entity-list-view-actions"}
+                >
+                  <Button aria-label="delete" size="small">
+                    <PencilIcon fontSize="small"/>
+                  </Button>
+                  <Button aria-label="delete" size="small">
+                    <DeleteIcon fontSize="small"/>
+                  </Button>
+                </ButtonGroup>
               </Grid>
               <Grid item xs={12}>
                 {EntityListView.renderProperties(entity)}
               </Grid>
-              <Grid item xs={12}>
-                {EntityListView.renderLinks(entity)}
-              </Grid>
+              {!expanded && <Button color={"primary"}
+                                    size={"medium"}
+                                    onClick={() => this.setState({expanded: true})}
+                                    startIcon={<DehazeIcon/>}>expand
+              </Button>}
+              {expanded && EntityListView.renderLinksAndBlobs(entity)}
             </Grid>
           </Grid>
         </Paper>
