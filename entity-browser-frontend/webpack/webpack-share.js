152371ec6eabb22fa10d6613c9fe6ef66fdb352c
lehvolk <alexey.volkov@jetbrains.com>
2022-02-02 15:28:20 +0300

    get rid of node-sass

--- entity-browser-frontend/webpack/webpack-share.js
+++ entity-browser-frontend/webpack/webpack-share.js
@@ -12,21 +12,7 @@ function getJavaScriptLoaders() {
 }
 function getStyleLoaders() {
     return [
-        {
-            test: /\.scss$/,
-            include: [path.resolve(projectDirectory, 'app')],
-            use: [
-                require.resolve('style-loader'),
-                require.resolve('css-loader'),
-                require.resolve('autoprefixer'),
-                {
-                    loader: require.resolve('sass-loader'),
-                    options: {
-                        outputStyle: 'expanded'
-                    }
-                }
-            ]
-        }, {
+       {
             test: /\.css$/,
             include: [
                 path.resolve(projectDirectory, 'app'),
